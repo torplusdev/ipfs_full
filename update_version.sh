d9f092e73257d572259540d5165bfd2a308568a9
Marat Tukhbatullin <>
2021-03-29 12:01:42 +0300

    fix

--- update_version.sh
+++ update_version.sh
@@ -18,16 +18,6 @@ multiaddrVersion=$(cd go-ipfs && git show refs/tags/${ipfsVersion}:go.mod | grep
 ipfsConfigVersion=$(cd go-ipfs && git show refs/tags/${ipfsVersion}:go.mod | grep ipfs/go-ipfs-config\ v | awk '{print $2}')
 eventbusVersion=$(cd go-libp2p && git show refs/tags/${libp2pVersion}:go.mod | grep libp2p/go-eventbus\ v | awk '{print $2}')
 
-
-echo "go-ipfs => ${ipfsVersion}"
-echo "go-libp2p-core => ${libp2pCoreVersion}"
-echo "go-libp2p => ${libp2pVersion}"
-echo "go-bitswap => ${bitswapVersion}"
-echo "go-libp2p-swarm => ${libp2pSwarmVersion}" 
-echo "go-multiaddr => ${multiaddrVersion}" 
-echo "go-ipfs-config => ${ipfsConfigVersion}" 
-echo "go-eventbus => ${eventbusVersion}"
-
 echo "Need rebase:"
 
 if [ -z $(cd go-ipfs && git tag --merged pp_master | grep ^v${ipfsVersion}$) ]; then echo "go-ipfs => ${ipfsVersion}"; fi
@@ -52,6 +42,22 @@ do
     fi
     cd ../
 done
+if [ -z $(cd go-ipfs && git tag --merged pp_master | grep ^v${ipfsVersion}$) ]; 
+  then echo "go-ipfs => ${ipfsVersion}"; fi
+if [ -z $(cd go-libp2p-core && git tag --merged pp_master | grep ^v${libp2pCoreVersion}$) ]; 
+  then echo "go-libp2p-core => ${libp2pCoreVersion}"; fi
+if [ -z $(cd go-libp2p && git tag --merged pp_master | grep ^v${libp2pVersion}$) ]; 
+  then echo "go-libp2p => ${libp2pVersion}"; fi
+if [ -z $(cd go-bitswap && git tag --merged pp_master | grep ^v${bitswapVersion}$) ]; 
+  then echo "go-bitswap => ${bitswapVersion}"; fi
+if [ -z $(cd go-libp2p-swarm && git tag --merged pp_master | grep ^v${libp2pSwarmVersion}$) ]; 
+  then echo "go-libp2p-swarm => ${libp2pSwarmVersion}"; fi
+if [ -z $(cd go-multiaddr && git tag --merged pp_master | grep ^v${multiaddrVersion}$) ]; 
+  then echo "go-multiaddr => ${multiaddrVersion}"; fi
+if [ -z $(cd go-ipfs-config && git tag --merged pp_master | grep ^v${ipfsConfigVersion}$) ]; 
+  then echo "go-ipfs-config => ${ipfsConfigVersion}"; fi
+if [ -z $(cd go-eventbus && git tag --merged pp_master | grep ^v${eventbusVersion}$) ]; 
+  then echo "go-eventbus => ${eventbusVersion}"; fi
 
 (cd go-ipfs && git rebase ${ipfsVersion} && cd ../  )
 (cd go-libp2p-core && git rebase ${libp2pCoreVersion} && cd ../  )
