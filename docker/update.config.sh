f8fe88641c14a6a9bf7e6d41bc0bfaa5b5836fbb
Marat Tukhbatullin <tumarsal@gmail.com>
2021-09-13 16:35:57 +0300

    fix config

--- docker/update.config.sh
+++ docker/update.config.sh
@@ -1,24 +1,24 @@
 
 file="ipfs.${PP_ENV}.cfg"
+
 if [[ "${PP_ENV}" = "stage" ]]; then
     hostNamePath=/var/lib/tor/hidden_service_v3/hostname
-    bootstrapServer="13.95.67.71"
-   
+    bootstrapServer="edikk202@13.95.67.71"
 fi
+
 if [[ "${PP_ENV}" = "prod" ]]; then
-    hostNamePath=/var/lib/tor/hidden_service_paidpiper/hostname
-    bootstrapServer="40.68.195.206"
-   
+    hostNamePath=/var/lib/tor/hidden_service_v3/hostname
+    bootstrapServer="TorPlus@172.104.247.185"
 fi
-ssh edikk202@${bootstrapServer} "touch ~/.hushlogin"
+ssh ${bootstrapServer} "touch ~/.hushlogin"
 
 echo "${PP_ENV}"
 
 #cat /var/lib/tor/hidden_service_v3/hostname
 #/var/lib/tor/hidden_service_paidpiper/
-fullOnionAddress=$(ssh edikk202@${bootstrapServer} "cat ${hostNamePath}")
-hsHostname=$(ssh edikk202@${bootstrapServer} "sed 's/[.].*$//' ${hostNamePath}")
-ipfsSuperPeerID=$(ssh edikk202@${bootstrapServer} "cat ~/.ipfs/config | jq -r '.Identity.PeerID'")
+fullOnionAddress=$(ssh ${bootstrapServer} "cat ${hostNamePath}")
+hsHostname=$(ssh ${bootstrapServer} "sed 's/[.].*$//' ${hostNamePath}")
+ipfsSuperPeerID=$(ssh ${bootstrapServer} "cat ~/.ipfs/config | jq -r '.Identity.PeerID'")
 
 
 echo "hsHostname=${hsHostname}" > $file
