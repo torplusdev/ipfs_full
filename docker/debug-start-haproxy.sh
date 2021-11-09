2b434e8b07563ad13498cfbb788f4c31a792eed4
Marat Tukhbatullin <tumarsal@gmail.com>
2021-11-09 21:45:12 +0300

    check ssl bash script

--- docker/debug-start-haproxy.sh
+++ docker/debug-start-haproxy.sh
@@ -4,6 +4,16 @@ if [[ "${useNginx}" == "1" ]]; then
         nginx   
 fi
 
+function checkPEM {
+        domain=$1       
+        if [[ "$(openssl x509 -noout -subject -in /etc/ssl/torplus/${domain}.pcm | awk -F = '{print $3}')" != "${domain}" ]]; then 
+                exit 1
+        fi
+}
+
+ls /etc/ssl/torplus/ | xargs -I {} checkPEM {}
+
+
 /pg-docker-entrypoint.sh &
 /tor-docker-entrypoint.sh &
 /haproxy-docker-entrypoint.sh &
