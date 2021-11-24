718625014f92f9e633f5d393ee744ec0f97adf49
Marat Tukhbatullin <tumarsal@gmail.com>
2021-11-24 12:39:44 +0300

    Fix start script

--- docker/debug-start-haproxy.sh
+++ docker/debug-start-haproxy.sh
@@ -7,10 +7,14 @@ fi
 function checkPEM {
         domain=$1       
         if [[ "$(openssl x509 -noout -subject -in /etc/ssl/torplus/${domain}.pcm | awk -F = '{print $3}')" != "${domain}" ]]; then 
+                echo "Certificate for ${domain} not valid."
                 exit 1
         fi
 }
-
+if [ -z "$(ls -A /etc/ssl/torplus/)" ]; then
+   echo "ssl directory empty"
+   exit .
+fi
 ls -1 /etc/ssl/torplus/ | sed -e 's/\.pem$//' | xargs -I {} checkPEM {}
 
 
